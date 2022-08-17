acb13529971d927aff6fa3f07ade51c3c73f35ee
arcimiendar <artimendor@yandex.ru>
2022-08-17 17:11:39 +0200

    testes functors

---
+++ tests/unit/test_functors.py
@@ -0,0 +1,27 @@
+from contextlib import suppress
+
+from app.domain.calculation import functors, OperationOptions, MathError
+
+
+def test_add_functor():
+    r = functors[OperationOptions.add](1, 2)
+    assert r == 3, f'"Add" functor is not working, got {r}, expected 3'
+
+
+def test_sub_functor():
+    r = functors[OperationOptions.sub](1, 2)
+    assert r == -1, f'"Sub" functor is not working, got {r}, expectet -1'
+
+
+def test_mul_functor():
+    r = functors[OperationOptions.mul](5, 6)
+    assert r == 30, f'"Mul" functor is not working, got {r}, expectet 30'
+
+
+def test_div_functor():
+    r = functors[OperationOptions.div](1, 2)
+    assert r == 0.5, f'"Div" functor is not working, got {r}, expectet 0.5'
+
+    with suppress(MathError):
+        r = functors[OperationOptions.div](1, 0)
+        assert False, f'"Div" functor is not working properly. Expected error, got  {r}'
